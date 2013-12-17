202f163eca54155063431c55b8842775d5ca3f6e
saurabh misra <saurabh_misra@dell.com>
2013-12-17 18:40:57 +0000

    Removed hard coded path for compellent library

--- lib/puppet/util/network_device/compellent/device.rb
+++ lib/puppet/util/network_device/compellent/device.rb
@@ -4,18 +4,19 @@ require 'puppet/util/network_device/transport_compellent'
 require 'uri'
 require 'net/https'
 
+#require_relative 'ResponseParser'
+#require 'puppet/util/network_device/ResponseParser'
+require 'puppet/lib/ResponseParser'
+
 class Puppet::Util::NetworkDevice::Compellent::Device
 
   attr_accessor :url, :transport
-
   def initialize(url, option = {})
     Puppet.debug("Device login started")
     @url = URI.parse(url)
     redacted_url = @url.dup
     redacted_url.password = "****" if redacted_url.password
-
-    Puppet.debug("Puppet::Device::Netapp: connecting to Netapp device #{redacted_url}")
-
+    Puppet.debug("Puppet::Device::Compellent: connecting to Compellent device #{redacted_url}")
     raise ArgumentError, "Invalid scheme #{@url.scheme}. Must be https" unless @url.scheme == 'https'
     raise ArgumentError, "no user specified" unless @url.user
     raise ArgumentError, "no password specified" unless @url.password
@@ -25,15 +26,42 @@ class Puppet::Util::NetworkDevice::Compellent::Device
     @transport.user = @url.user
     @transport.password = @url.password
     Puppet.debug("host is #{@transport.host}")
-    response = system("java -jar /etc/puppet/modules/compellent/lib/puppet/util/network_device/compellent/CompCU-6.3.jar -host  #{@url.host} -user #{@url.user} -password P@ssw0rd -xmloutputfile /tmp/#{@url.host}_loginExitCode.xml -c \"system show -xml /tmp/#{@url.host}_loginResponse.xml\" ")
-    Puppet.debug("the end")
+    libpath = get_path(3)
+    response = system("java -jar #{libpath} -host  #{@url.host} -user #{@url.user} -password #{@url.password} -xmloutputfile /tmp/#{@url.host}_loginExitCode.xml -c \"system show -xml /tmp/#{@url.host}_loginResponse.xml\" ")
+    parserObj=ResponseParser.new('_')
+    file_path = "/tmp/#{@transport.host}_loginExitCode.xml"
+    parserObj.parse_exitcode(file_path)
+    hash= parserObj.return_response
+    if "#{hash['Success']}".to_str() == "TRUE"
+      Puppet.debug("Login successful..")
+    else
+      raise Puppet::Error, "#{hash['Error']}"
+    end
   end
-		
+
+  def get_path(num)
+    temp_path = Pathname.new(__FILE__).parent
+    Puppet.debug("Temp PATH - #{temp_path}")
+    $i = 0
+    $num = num
+    p = Pathname.new(temp_path)
+    while $i < $num  do
+      p = Pathname.new(temp_path)
+      temp_path = p.dirname
+      $i +=1
+    end
+    temp_path = temp_path.join('lib/CompCU-6.3.jar')
+    Puppet.debug("Path #{temp_path}")
+    return  temp_path
+  end
+
   def facts
     Puppet.debug("In facts")
     @facts ||= Puppet::Util::NetworkDevice::Compellent::Facts.new(@transport)
     facts = @facts.retrieve
-    
+
     facts
   end
 end
+
+
--- lib/puppet/util/network_device/compellent/facts.rb
+++ lib/puppet/util/network_device/compellent/facts.rb
@@ -1,6 +1,7 @@
 require 'puppet/util/network_device/compellent'
 require 'puppet/util/network_device/transport_compellent'
 require 'rexml/document'
+require 'puppet/lib/ResponseParser'
 
 include REXML
 
@@ -15,83 +16,29 @@ class Puppet::Util::NetworkDevice::Compellent::Facts
     @transport = transport
   end
 
-  def read_node(node,key_name)
-    if node.has_elements?
-      node.each_element do |child|
-        read_node(child,key_name)
-      end
-    else
-      key_name="#{key_name}#{@seperator}#{node.name()}"
-      self.facts[key_name]=node.text
+  def get_path(num)
+    temp_path = Pathname.new(__FILE__).parent
+    Puppet.debug("Temp PATH #{temp_path}")
+    $i = 0
+    $num = num
+    p = Pathname.new(temp_path)
+    while $i < $num  do
+      p = Pathname.new(temp_path)
+      temp_path = p.dirname
+      $i +=1
     end
-    key_name
+    temp_path = temp_path.join('lib/CompCU-6.3.jar')
+    Puppet.debug("Final Lib Path #{temp_path}")
+    return temp_path
   end
-
-  def parse_discovery(result_file_name, output_file_name,index)
-    result_file = File.new(result_file_name)
-    result_doc = Document.new(result_file)
-    result= XPath.first(result_doc, "//Success")
-    if result.text.eql?'TRUE'
-      result_file = File.new(output_file_name)
-      output_doc = Document.new(result_file)
-      #index=1;
-      output_doc.root.each_element do |system|
-        if index > 0
-          key= "#{system.name()}#{@seperator}#{index}"
-        else
-          key= "#{system.name()}"
-        end
-        read_node(system,key)
-        index=index+1
-      end
-    end
-    @facts
-  end
-
-def get_index_value(node)
-    node.elements["Index"].text
-  end
-
-  def read_diskfolder_node(node,key_name)
-    if node.has_elements?
-      node.each_element do |child|
-        read_diskfolder_node(child,key_name)
-      end
-    else
-      if node.parent.name().eql?"StorageType" then  key_name="#{key_name}#{@seperator}StorageType#{@seperator}#{get_index_value(node.parent)}#{@seperator}#{node.name()}"
-      else
-        key_name="#{key_name}#{@seperator}#{node.name()}"
-      end
-      self.facts[key_name]=node.text
-    end
-    key_name
-  end
-
- def parse_diskfolder_xml(result_file_name, output_file_name)
-    result_file = File.new(result_file_name)
-    result_doc = Document.new(result_file)
-    result= XPath.first(result_doc, "//Success")
-    if result.text.eql?'TRUE'
-      result_file = File.new(output_file_name)
-      output_doc = Document.new(result_file)
-      index=1;
-      output_doc.root.each_element do |system|
-        key= "#{system.name()}#{@seperator}#{index}"
-        read_diskfolder_node(system,key)
-        index=index+1
-      end
-    end
-    @facts
-  end
-
 
   def retrieve
+    libpath = get_path(3)
     Puppet.debug("In facts retrieve")
     Puppet.debug("IP Address is #{@transport.host} Username is #{@transport.user} Password is #{@transport.password}")
-    response = system("java -jar /etc/puppet/modules/compellent/lib/puppet/util/network_device/compellent/CompCU-6.3.jar -host #{@transport.host} -user #{@transport.user} -password P@ssw0rd -xmloutputfile /tmp/#{@transport.host}_systemExitCode.xml -c \"system show -xml /tmp/#{@transport.host}_systemResponse.xml\" ")
-    response = system("java -jar /etc/puppet/modules/compellent/lib/puppet/util/network_device/compellent/CompCU-6.3.jar -host #{@transport.host} -user #{@transport.user} -password P@ssw0rd -xmloutputfile /tmp/#{@transport.host}_controllerExitCode.xml -c \"controller show -xml /tmp/#{@transport.host}_controllerResponse.xml\" ")
-    response = system("java -jar /etc/puppet/modules/compellent/lib/puppet/util/network_device/compellent/CompCU-6.3.jar -host #{@transport.host} -user #{@transport.user} -password P@ssw0rd -xmloutputfile /tmp/#{@transport.host}_diskfolderExitCode.xml -c \"diskfolder show -xml /tmp/#{@transport.host}_diskfolderResponse.xml\" ")
- #  response = system("java -jar /etc/puppet/modules/compellent/lib/puppet/util/network_device/compellent/CompCU-6.3.jar -host #{@transport.host} -user #{@transport.user} -password P@ssw0rd -xmloutputfile /tmp/#{@transport.host}_alertExitCode.xml -c \"alert show -xml /tmp/#{@transport.host}_alertResponse.xml\" ")
+    response = system("java -jar #{libpath} -host #{@transport.host} -user #{@transport.user} -password #{@transport.password} -xmloutputfile /tmp/#{@transport.host}_systemExitCode.xml -c \"system show -xml /tmp/#{@transport.host}_systemResponse.xml\" ")
+    response = system("java -jar #{libpath} -host #{@transport.host} -user #{@transport.user} -password #{@transport.password} -xmloutputfile /tmp/#{@transport.host}_controllerExitCode.xml -c \"controller show -xml /tmp/#{@transport.host}_controllerResponse.xml\" ")
+    response = system("java -jar #{libpath} -host #{@transport.host} -user #{@transport.user} -password #{@transport.password} -xmloutputfile /tmp/#{@transport.host}_diskfolderExitCode.xml -c \"diskfolder show -xml /tmp/#{@transport.host}_diskfolderResponse.xml\" ")
 
     resp1 = "/tmp/#{@transport.host}_systemExitCode.xml"
     resp2 = "/tmp/#{@transport.host}_systemResponse.xml"
@@ -105,15 +52,13 @@ def get_index_value(node)
     resp7 = "/tmp/#{@transport.host}_alertExitCode.xml"
     resp8 = "/tmp/#{@transport.host}_alertResponse.xml"
 
-
-    parse_discovery(resp1,resp2,0)
-    parse_discovery(resp3,resp4,1)
-    parse_diskfolder_xml(resp5,resp6)
-#   parse_discovery(resp7,resp8,1)
-
-    @facts
+    Puppet.debug("Creating Parser Object")
+    parserObj=ResponseParser.new('_')
+    parserObj.parse_discovery(resp1,resp2,0)
+    parserObj.parse_discovery(resp3,resp4,1)
+    parserObj.parse_diskfolder_xml(resp5,resp6)
+    @facts =  parserObj.return_response
   end
 
 end
-
 
