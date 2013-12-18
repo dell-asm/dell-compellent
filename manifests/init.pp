8f5e4900d9b345ec4388f414249003b7af432741
saurabh misra <saurabh_misra@dell.com>
2013-12-18 12:05:34 +0000

    Updated default init file to not to invoke any operation

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,17 +13,5 @@
 # [Remember: No empty lines between comments and class definition]
 
 class compellent{
- compellent::vqe { 'Test1':
-  user              => 'test',
-  password          => 'P@ssw0rd',
-  host              => '172.17.10.45',
-  size 				=> '2g',
-  ensure        	=> 'absent',
-  boot				=>  false,  
-  folder            => 'folder2',
-  notes 			=> 'Test1',
-  replayprofile 	=> 'Sample',
-  storageprofile	=> 'Low Priority',
-    }
 }
 
