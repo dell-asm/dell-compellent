af91e46919ed361a81b52731eaa5479bc3ef7c6c
saurabh misra <saurabh_misra@dell.com>
2013-12-18 10:38:51 +0000

    code cleanup

--- lib/puppet/util/network_device/transport_compellent.rb
+++ lib/puppet/util/network_device/transport_compellent.rb
@@ -2,7 +2,7 @@
 
 require 'net/https'
 require 'puppet/util/network_device'
-class Puppet::Util::NetworkDevice::TransportCompellent
+class Puppet::Util::NetworkDevice::Transport_compellent
       attr_accessor :host, :user, :password
       def initialize
       end
