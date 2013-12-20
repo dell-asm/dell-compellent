3a9f0f99056f1de37ad5f05548de400ef1b323fc
saurabh misra <saurabh_misra@dell.com>
2013-12-20 09:50:00 +0000

    Added default init.pp

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+# Class: compellent
+#
+# This module manages compellent
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class compellent{
+}
+
