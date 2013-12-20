fe0c87de77a112a7be90cea8df8ee658070914e9
saurabh misra <saurabh_misra@dell.com>
2013-12-20 09:52:40 +0000

    sample init and pp files

---
+++ manifests/sample_hba_add_delete.pp
@@ -0,0 +1,59 @@
+# == Define: compellent::hba
+#
+# Utility class for creation of a Server HBA.
+#
+# === Parameters:
+#
+# [*ensure*]
+#   The resource state.
+#
+# [*wwn*]
+#   The World Wide Number.
+#
+# [*porttype*]
+#   The type of port.
+#
+# [*manual*]
+#   Space reservation mode. Valid options are: none, file and volume.# 
+#
+# === Actions:
+#
+# === Requires:
+#
+# === Sample Usage:
+#
+# Make sure there is a server hba called `server_hba`.
+#
+#  compellent::hba { 'server_hba':
+# 	user, 
+#	password,
+#	host,
+#	ensure        	= 'present',
+#	porttype	    = '1234',
+#	wwn	          	= 'WWN',
+#	manual			= false,
+#     }
+#
+
+define compellent::hba_add_delete (
+  $user, 
+  $password,
+  $host,
+  $wwn,
+  $ensure        	= 'present',
+  $porttype	        = '',
+  $serverfolder 	= '',
+  $manual		    =  false,  
+  
+) {
+  compellent_hba_add_delete { "${name}":
+  ensure       	 	 => $ensure,
+  porttype		     => $porttype,
+  wwn	          	 => $wwn,
+  manual		     => $manual,
+  user			     => $user,
+  password 		     => $password,
+  serverfolder       => $serverfolder,
+  host			     => $host,
+  }
+}
