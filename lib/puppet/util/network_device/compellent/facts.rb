bae7829e4b7d768b160adebfcdd919390d59fde3
saurabh misra <saurabh_misra@dell.com>
2013-12-24 07:15:38 +0000

    Updated discovery to populate required reporting data in JSON format

--- lib/puppet/util/network_device/compellent/facts.rb
+++ lib/puppet/util/network_device/compellent/facts.rb
@@ -3,6 +3,10 @@ require 'puppet/util/network_device/transport_compellent'
 require 'rexml/document'
 require 'puppet/lib/ResponseParser'
 
+require 'json'
+require 'xmlsimple'
+
+
 include REXML
 
 class Puppet::Util::NetworkDevice::Compellent::Facts
@@ -81,10 +85,23 @@ class Puppet::Util::NetworkDevice::Compellent::Facts
     ctrl_exitcodexml = "#{get_log_path(3)}/ctrlExitCode_#{get_unique_refid}.xml"
     diskfolder_respxml = "#{get_log_path(3)}/diskfolderResp_#{get_unique_refid}.xml"
     diskfolder_exitcodexml = "#{get_log_path(3)}/diskfolderExitCode_#{get_unique_refid}.xml"
+    volume_respxml = "#{get_log_path(3)}/volumeResp_#{get_unique_refid}.xml"
+    volume_exitcodexml = "#{get_log_path(3)}/volumeExitCode_#{get_unique_refid}.xml"
+    server_respxml = "#{get_log_path(3)}/serverResp_#{get_unique_refid}.xml"
+    server_exitcodexml = "#{get_log_path(3)}/serverExitCode_#{get_unique_refid}.xml"
+    replayprofile_respxml = "#{get_log_path(3)}/replayprofileResp_#{get_unique_refid}.xml"
+    replayprofile_exitcodexml = "#{get_log_path(3)}/replayprofileExitCode_#{get_unique_refid}.xml"
+    storageprofile_respxml = "#{get_log_path(3)}/storageprofileResp_#{get_unique_refid}.xml"
+    storageprofile_exitcodexml = "#{get_log_path(3)}/storageprofileExitCode_#{get_unique_refid}.xml"
+    
     
     response = system("java -jar #{libpath} -host #{@transport.host} -user #{@transport.user} -password #{@transport.password} -xmloutputfile #{system_exitcodexml} -c \"system show -xml #{system_respxml}\" ")
     response = system("java -jar #{libpath} -host #{@transport.host} -user #{@transport.user} -password #{@transport.password} -xmloutputfile #{ctrl_exitcodexml} -c \"controller show -xml #{ctrl_respxml}\" ")
     response = system("java -jar #{libpath} -host #{@transport.host} -user #{@transport.user} -password #{@transport.password} -xmloutputfile #{diskfolder_exitcodexml} -c \"diskfolder show -xml #{diskfolder_respxml}\" ")
+    response = system("java -jar #{libpath} -host #{@transport.host} -user #{@transport.user} -password #{@transport.password} -xmloutputfile #{volume_exitcodexml} -c \"volume show -xml #{volume_respxml}\" ")
+    response = system("java -jar #{libpath} -host #{@transport.host} -user #{@transport.user} -password #{@transport.password} -xmloutputfile #{server_exitcodexml} -c \"server show -xml #{server_respxml}\" ")
+    response = system("java -jar #{libpath} -host #{@transport.host} -user #{@transport.user} -password #{@transport.password} -xmloutputfile #{replayprofile_exitcodexml} -c \"replayprofile show -xml #{replayprofile_respxml}\" ")
+    response = system("java -jar #{libpath} -host #{@transport.host} -user #{@transport.user} -password #{@transport.password} -xmloutputfile #{storageprofile_exitcodexml} -c \"storageprofile show -xml #{storageprofile_respxml}\" ")
 
     Puppet.debug("Creating Parser Object")
     parser_obj=ResponseParser.new('_')
@@ -92,6 +109,16 @@ class Puppet::Util::NetworkDevice::Compellent::Facts
     parser_obj.parse_discovery(ctrl_exitcodexml,ctrl_respxml,1)
     parser_obj.parse_diskfolder_xml(diskfolder_exitcodexml,diskfolder_respxml)
     @facts =  parser_obj.return_response
+    self.facts["system_data"]=JSON.pretty_generate(XmlSimple.xml_in(system_respxml))
+    self.facts["diskfolder_data"]=JSON.pretty_generate(XmlSimple.xml_in(diskfolder_respxml))
+    self.facts["controller_data"]=JSON.pretty_generate(XmlSimple.xml_in(ctrl_respxml))
+    self.facts["volume_data"]=JSON.pretty_generate(XmlSimple.xml_in(volume_respxml))
+    self.facts["server_data"]=JSON.pretty_generate(XmlSimple.xml_in(server_respxml))
+    self.facts["replayprofile_data"]=JSON.pretty_generate(XmlSimple.xml_in(replayprofile_respxml))
+    self.facts["storageprofile_data"]=JSON.pretty_generate(XmlSimple.xml_in(storageprofile_respxml))
+
+    
+    @facts
   end
 
 end
