a7defa1805de300677e92b86f48918c7a113c0a7
Santosh Kumar13 <Santosh_Kumar13@dell.com>
2014-01-09 06:47:24 +0000

    compellent code refactoring Reviewed by Vipin

--- manifests/hba.pp
+++ manifests/hba.pp
@@ -2,40 +2,14 @@
 #
 # Utility class for creation of a Server HBA.
 #
-# === Parameters:
-#
-# [*ensure*]
-#   The resource state.
-#
-# [*wwn*]
-#   The World Wide Number.
-#
-# [*porttype*]
-#   The type of port.
-#
-# [*manual*]
-#   Space reservation mode. Valid options are: none, file and volume.#
-#
-# === Actions:
-#
-# === Requires:
-#
-# === Sample Usage:
-#
-# Make sure there is a server hba called `server_hba`.
-#
-#  compellent::hba { 'server_hba':
-# 	user,
-# password,
-# host,
-# ensure        	= 'present',
-# porttype	    = '1234',
-# wwn	          	= 'WWN',
-# manual			= false,
-#     }
-#
 
-define compellent::hba ($wwn, $ensure = 'present', $porttype = '', $serverfolder = '', $manual = false,) {
+
+define compellent::hba (
+  $wwn, 
+  $ensure = 'present', 
+  $porttype = '', 
+  $serverfolder = '', 
+  $manual = false,) {
   compellent_hba { "$name":
     ensure       => $ensure,
     porttype     => $porttype,
